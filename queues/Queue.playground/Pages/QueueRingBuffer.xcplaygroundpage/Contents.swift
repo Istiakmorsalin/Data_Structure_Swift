84271df017a503f9e6d6a6c1c1881f5f0b31f646
Istiak Morsalin <anik.cse@gmail.com>
2018-12-04 14:34:06 +0600

    Subject: Queue [Description]: -  Basic queue implemented - Queue implemented with array, double linked list & Ring Buffer Signed-off-by: Istiak Morsalin <anik.cse@gmail.com>

---
+++ queues/Queue.playground/Pages/QueueRingBuffer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+
+// Istiak
+
+public struct QueueRingBuffer<T>: Queue {
+  
+  private var ringBuffer: RingBuffer<T>
+  
+  public init(count: Int) {
+    ringBuffer = RingBuffer<T>(count: count)
+  }
+  
+  public var isEmpty: Bool {
+    return ringBuffer.isEmpty
+  }
+  
+  public var peek: T? {
+    return ringBuffer.first
+  }
+  
+  public mutating func enqueue(_ element: T) -> Bool {
+    return ringBuffer.write(element)
+  }
+  
+  public mutating func dequeue() -> T? {
+    return isEmpty ? nil : ringBuffer.read()
+  }
+}
+
+extension QueueRingBuffer: CustomStringConvertible {
+  
+  public var description: String {
+    return String(describing: ringBuffer)
+  }
+}
+
+var queue = QueueRingBuffer<String>(count: 10)
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+queue.enqueue("Eric")
+queue
+queue.dequeue()
+queue
+queue.peek
